e49d53a6c8585912bd35fb197479e3501b93599d
rupali dudhal <rupalidudhal95@gmail.com>
2025-04-29 19:31:11 +0530

    navbar and hero section

---
+++ my-app/src/components/Button/Button.jsx
@@ -0,0 +1,14 @@
+export default function Button() {
+  <button
+    style={{
+      fontFamily: "Poppins",
+      backgroundColor: "black",
+      borderRadius: "5px",
+      color: "#34C94B",
+      borderLeft: "2px solid #34C94B ",
+      borderRight: "2px solid #34C94B ",
+    }}
+  >
+    Give Feedback
+  </button>;
+}
